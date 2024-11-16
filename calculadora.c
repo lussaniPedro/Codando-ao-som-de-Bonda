670f5d6fbd221cf334ae91febc01229b62310ace
Pedro Henrique <p.henriquelussani@gmail.com>
2024-11-16 10:14:42 -0300

    Calculadora em C

--- calculadora.c
+++ calculadora.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void menu();
-int OpcaoCalcular();
-int calcular(int op, double num1, double num2);
-void exibirHistorico();
-
-double *_result;
-int _historico = 0;
+void calcular(int op, float num1, float num2);
 
 int main(){
-    menu();
-    return 0;
-}
-
-void menu(){
+    int op;
+    float num1, num2;
 
     do{
         system("cls");
@@ -23,98 +14,56 @@ void menu(){
         printf("- (2) Subtracao\n");
         printf("- (3) Multiplicacao\n");
         printf("- (4) Divisao\n");
-        printf("- (5) Historico\n");
-        printf("- (6) Sair\n\n");
-    } while(OpcaoCalcular() != 6);
+        printf("- (5) Sair\n");
+        printf("Digite a opcao: ");
+        scanf("%d", &op);
+
+        if(op >= 1 && op <= 4){
+            system("cls");
+            printf("Digite o primeiro numero: ");
+            scanf("%f", &num1);
+
+            printf("Digite o segundo numero: ");
+            scanf("%f", &num2);
+        }
+
+        system("cls");
+        calcular(op, num1, num2);
+    } while(op != 5);
+
+    return 0;
 }
 
-int OpcaoCalcular(){
-    int op;
-    double num1, num2;
+void calcular(int op, float num1, float num2){
+    float res;
 
-    printf("Digite a opcao: ");
-    scanf("%d", &op);
-
-    if(op >= 1 && op <= 4){
-        system("cls");
-        printf("Digite o primeiro numero: ");
-        scanf("%lf", &num1);
-
-        printf("Digite o segundo numero: ");
-        scanf("%lf", &num2);
-    }
-
-    if(op == 4 && num2 == 0){
-        system("cls");
-        printf("*** ERRO!! Divisao por 0 NAO permitida!! SEU VERME IMUNDO!!! ***\n");
-        printf("*** ERRO!! Divisao por 0 NAO permitida!! SEU VERME IMUNDO!!! ***\n");
-        printf("*** ERRO!! Divisao por 0 NAO permitida!! SEU VERME IMUNDO!!! ***\n");
-        exit(1);
-    }
-
-    if(_historico == 0){
-        _result = (double*)malloc(1 * sizeof(double));
-    } else{
-        _result = (double*)realloc(_result, (_historico + 1) * sizeof(double));
-    }
-    _result[_historico] = calcular(op, num1, num2);
-    _historico++;
-
-    return op;
-}
-
-int calcular(int op, double num1, double num2){
-    double res;
-
-    system("cls");
     switch(op){
         case 1:
             res = num1 + num2;
-            printf("O resultado da soma de %.1lf e %.1lf eh %.1lf\n", num1, num2, res);
+            printf("O resultado da soma de %.2f e %.2f eh %.2f\n", num1, num2, res);
             break;
         case 2:
             res = num1 - num2;
-            printf("O resultado da subtracao de %.1lf e %.1lf eh %.1lf\n", num1, num2, res);
+            printf("O resultado da subtracao de %.2f e %.2f eh %.2f\n", num1, num2, res);
             break;
         case 3:
             res = num1 * num2;
-            printf("O resultado da multiplicacao de %.1lf e %.1lf eh %.1lf\n", num1, num2, res);
+            printf("O resultado da multiplicacao de %.2f e %.2f eh %.2f\n", num1, num2, res);
             break;
         case 4:
-            res = num1 / num2;
-            printf("O resultado da divisao de %.1lf e %.1lf eh %.1lf\n", num1, num2, res);
+            if(op == 4 && num2 != 0){
+                res = num1 / num2;
+                printf("O resultado da divisao de %.2f e %.2f eh %.2f\n", num1, num2, res);
+            } else{
+                printf("Erro! divsao por 0 nao permitida!!!\n");
+            }
             break;
         case 5:
-            exibirHistorico();
-            break;
-        case 6:
             printf("Saindo...");
-            exit(0);
+            return;
         default:
-            printf("Opcao invalida!!!\n");
+            printf("Erro! Opcao invalida!!!\n");
             break;
     }
     system("pause");
-
-    return res;
-}
-
-void exibirHistorico(){
-    if(_historico == 0){
-        printf("** Historico vazio! **\n");
-        system("pause");
-        return;
-    }
-    printf("-- Historico --\n");
-    for(int i = 0; i < _historico; i++){
-        if(i < 10 && _result[i] < 10){
-            printf("[0%d]|  0%.1lf  |\n", i + 1, _result[i]);
-        } else if(i >= 10 && _result[i] < 10){
-            printf("[%d]|  0%.1lf  |\n", i + 1, _result[i]);
-        } else if(i < 10 && _result[i] >= 10){
-            printf("[0%d]|  %.1lf  |\n", i + 1, _result[i]);
-        } else{
-            printf("[%d]|  %.1lf  |\n", i + 1, _result[i]);
-        }
-    }
 }
